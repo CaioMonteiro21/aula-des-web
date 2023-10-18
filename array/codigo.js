a96cea28eda5db934ed32fb263f17f55e8f4d480
Caio Monteiro <142441252+CaioMonteiro21@users.noreply.github.com>
2023-10-18 15:19:08 -0300

    Add files via upload

--- array/codigo.js
+++ array/codigo.js
@@ -1,22 +1,39 @@
 const ingrid = atletas[0];
 
-window.onload = () => 
 
-
-
-const espaco_nome = document.getElementById('nome');
-const espaco_imagem = document.querySelector('div#container')
-const espaco_descricao = document.querySelector('div#container')
 
 const preenche = (atleta) => {
-      espaco_nome.innerText = atleta.nome;
-      espaco_imagem.src = atleta.imagem;
-      espaco_descricao.innerHTML = atleta.descricao;
+    const container = document.createElement('div');
+    const titulo = document.createElement('h3');
+    const imagem = document.createElement('img');
+    const descricao = document.createElement('p');
 
+    container.appendChild(titulo);
+    container.appendChild(imagem);
+    container.appendChild(descricao);
+
+document.body.appendChild(container);
+
+    titulo.innerText = atleta.nome;
+    imagem.src = atleta.imagem;
+    descricao.innerHTML = atleta.descricao;
+}
+/*
+atletas.foreach( (atleta) => {preenche(alteta)});
+*/
+
+/*let indice = 0;
+
+while (indice < atletas.length){
+  preenche(atleta);
+  console.log(indice, atletas[indice].nome)
+  indice++;
+
+}*/
+
+for ( let indice = 0; indice < atletas.length; indice++){
+    preenche(atletas[indice]);
+    console.log(indice, 'loop for')
 
 
 }
-
-
-
-
